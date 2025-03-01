a5491671c9c658747cb06938204857fd4fef3ad2
Gustavo Tonolli <gustavo.tonolli@hotmail.com>
2025-03-01 15:42:05 -0300

    feat(): variaveis

--- MyApp/Program.cs
+++ MyApp/Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿namespace MeuApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const int idade = 0;
+            var texto = "Testando";
+            Console.WriteLine(texto);
+        }
+    }
+}
