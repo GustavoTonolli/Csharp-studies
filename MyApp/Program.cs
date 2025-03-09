ff315e9b9d2076db8a3220348f531d4c44d9178b
Gustavo Tonolli <gustavo.tonolli@hotmail.com>
2025-03-09 13:22:45 -0300

    feat(): variables

--- MyApp/Program.cs
+++ MyApp/Program.cs
@@ -4,7 +4,18 @@
     {
         static void Main(string[] args)
         {
-            const int idade = 0;
+            sbyte mySbyte = -128;
+            short myShort = 32767;
+            long myLong = 9223372036854775807;
+            float myFloat = 3.40282347E+38F;
+            double myDouble = 1.7976931348623157E+308;
+            decimal myDecimal = 79228162514264337593543950335M;
+            char myChar = 'A';
+            bool myBool = true;
+            string myString = "Hello World!";
+            ulong myUlong = 18446744073709551615;
+            byte myByte = 255;
+            const int idade = 2147483647;
             var texto = "Testando";
             Console.WriteLine(texto);
         }
--- MyApp/obj/Debug/net9.0/MyApp.AssemblyInfo.cs
+++ MyApp/obj/Debug/net9.0/MyApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+61648cfb255104fc90d59b730ba0d30befbe51c4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a5491671c9c658747cb06938204857fd4fef3ad2")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MyApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
