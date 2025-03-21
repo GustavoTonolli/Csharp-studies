ae52b5494d47ee394fad649ecae59534775ba0d5
Gustavo Tonolli <gustavo.tonolli@hotmail.com>
2025-03-21 07:21:34 -0300

    calculadora

---
+++ Calculator/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Calculater
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Clear(); // limpar a tela
+            Console.WriteLine("Primeiro valor: ");
+            float v1 = float.Parse(Console.ReadLine());
+
+            Console.WriteLine(v1);
+        }
+    }
+}
